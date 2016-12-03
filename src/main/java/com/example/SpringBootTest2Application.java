ba31dd0e776ac7962a7c472ea131faf1e9d7ea3f
BlackDev <bartlomiejplonka@gmail.com>
2016-12-03 20:42:31 +0100

    First commit

---
+++ src/main/java/com/example/SpringBootTest2Application.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootTest2Application {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest2Application.class, args);
+	}
+}
